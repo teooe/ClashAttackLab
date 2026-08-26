c43072b4996c31ba783684bd44acad84f49baee6
Matteo Ragazzoli <matteogiovanniragazzoli@gmail.com>
2026-08-26 13:34:01 +0200

    Initial Commit

---
+++ ClashAttackLab/ClashAttackLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClashAttackLabApp.swift
+//  ClashAttackLab
+//
+//  Created by Matteo Ragazzoli on 26/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClashAttackLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClashAttackLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClashAttackLab
+//
+//  Created by Matteo Ragazzoli on 26/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
