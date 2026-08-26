9c76f8d0d6c7201f3924354f693902853c540731
teooe <matteogiovanniragazzoli@gmail.com>
2026-08-26 22:05:42 +0200

    Add simulation lifecycle and result

--- ClashAttackLabMac/Simulation/SimulationStatus.swift
+++ ClashAttackLabMac/Simulation/SimulationStatus.swift
@@ -1,12 +1,33 @@
 import Foundation
 
-enum SimulationStatus {
-    case ready
+enum BattleWinner {
+    case giant
+    case cannon
+    case draw
 
     var displayName: String {
         switch self {
-        case .ready:
-            return "pronto"
+        case .giant:
+            return "Gigante"
+        case .cannon:
+            return "Cannone"
+        case .draw:
+            return "Pareggio"
         }
     }
+}
+
+struct SimulationResult {
+    let winner: BattleWinner
+    let elapsedTime: TimeInterval
+    let giantRemainingHitPoints: Double
+    let cannonRemainingHitPoints: Double
+    let giantAttackCount: Int
+    let cannonAttackCount: Int
+}
+
+enum SimulationStatus {
+    case ready
+    case running
+    case finished(SimulationResult)
 }
