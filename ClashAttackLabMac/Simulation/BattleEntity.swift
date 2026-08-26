21707e6b9b3849c5dbced86f026ee555696a9cb8
teooe <matteogiovanniragazzoli@gmail.com>
2026-08-26 22:24:57 +0200

    Support buildings and blocking walls

--- ClashAttackLabMac/Simulation/BattleEntity.swift
+++ ClashAttackLabMac/Simulation/BattleEntity.swift
@@ -8,6 +8,9 @@ struct WorldPosition: Equatable {
 enum BattleEntityKind: Hashable {
     case giant
     case cannon
+    case townHall
+    case goldStorage
+    case wall
 }
 
 struct BattleEntity: Identifiable {
@@ -16,7 +19,12 @@ struct BattleEntity: Identifiable {
     var position: WorldPosition
     var hitPoints: Double
     var attackCooldown: TimeInterval
+
+    /// Main building selected through target-selection rules.
     var currentTargetID: UUID?
+
+    /// Temporary wall that blocks the route to the main target.
+    var blockingWallID: UUID?
 
     init(
         id: UUID = UUID(),
@@ -24,7 +32,8 @@ struct BattleEntity: Identifiable {
         position: WorldPosition,
         hitPoints: Double = 0,
         attackCooldown: TimeInterval = 0,
-        currentTargetID: UUID? = nil
+        currentTargetID: UUID? = nil,
+        blockingWallID: UUID? = nil
     ) {
         self.id = id
         self.kind = kind
@@ -32,6 +41,7 @@ struct BattleEntity: Identifiable {
         self.hitPoints = hitPoints
         self.attackCooldown = attackCooldown
         self.currentTargetID = currentTargetID
+        self.blockingWallID = blockingWallID
     }
 
     var isAlive: Bool {
