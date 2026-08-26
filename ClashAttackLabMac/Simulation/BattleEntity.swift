cf21d5a0146008ce653e28a67d473298768ae7b7
teooe <matteogiovanniragazzoli@gmail.com>
2026-08-26 22:12:17 +0200

    Track each entity target

--- ClashAttackLabMac/Simulation/BattleEntity.swift
+++ ClashAttackLabMac/Simulation/BattleEntity.swift
@@ -16,19 +16,22 @@ struct BattleEntity: Identifiable {
     var position: WorldPosition
     var hitPoints: Double
     var attackCooldown: TimeInterval
+    var currentTargetID: UUID?
 
     init(
         id: UUID = UUID(),
         kind: BattleEntityKind,
         position: WorldPosition,
         hitPoints: Double = 0,
-        attackCooldown: TimeInterval = 0
+        attackCooldown: TimeInterval = 0,
+        currentTargetID: UUID? = nil
     ) {
         self.id = id
         self.kind = kind
         self.position = position
         self.hitPoints = hitPoints
         self.attackCooldown = attackCooldown
+        self.currentTargetID = currentTargetID
     }
 
     var isAlive: Bool {
