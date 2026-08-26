3b99217518237c15fc4b307cf4e8ede6fa119860
Matteo Ragazzoli <matteogiovanniragazzoli@gmail.com>
2026-08-26 19:50:50 +0200

    Add native macOS target

---
+++ ClashAttackLabMac/ClashAttackLabMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClashAttackLabMacApp.swift
+//  ClashAttackLabMac
+//
+//  Created by Matteo Ragazzoli on 26/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClashAttackLabMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClashAttackLabMacTests/ClashAttackLabMacTests.swift
@@ -0,0 +1,19 @@
+//
+//  ClashAttackLabMacTests.swift
+//  ClashAttackLabMacTests
+//
+//  Created by Matteo Ragazzoli on 26/08/2026.
+//
+
+import Testing
+@testable import ClashAttackLabMac
+
+struct ClashAttackLabMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
