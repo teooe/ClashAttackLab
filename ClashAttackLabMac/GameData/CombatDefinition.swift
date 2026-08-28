d71e64bbde1416e1a3aaad14fd8cff1d82c9c988
teooe <matteogiovanniragazzoli@gmail.com>
2026-08-28 08:33:05 +0200

    Model targeting preferences and evidence

--- ClashAttackLabMac/GameData/CombatDefinition.swift
+++ ClashAttackLabMac/GameData/CombatDefinition.swift
@@ -7,6 +7,23 @@ enum BattleEntityRole: Hashable {
     case wall
 }
 
+enum MechanicEvidence: String, Hashable {
+    case documented
+    case observed
+    case approximation
+    case prototype
+}
+
+enum TargetPreference: Hashable {
+    case defenses
+    case anyBuilding
+}
+
+struct TargetingProfile {
+    let preference: TargetPreference
+    let evidence: MechanicEvidence
+}
+
 struct CombatDefinition {
     let displayName: String
     let role: BattleEntityRole
@@ -16,6 +33,9 @@ struct CombatDefinition {
     let attackRange: Double
     let attackInterval: TimeInterval
     let canMove: Bool
+
+    /// Nil for entities that do not choose offensive building targets.
+    let targetingProfile: TargetingProfile?
 
     var countsForDestruction: Bool {
         role == .defense || role == .building
